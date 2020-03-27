2e5cd3f636843c6ad7e73a4568ddf7ca08a427d2
abhishekkushwaha4u <abhishek.kushwaha2018@vitstudent.ac.in>
2020-03-27 20:01:53 +0530

    modified project structure

--- hestia/hestia/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('user/report/', views.ReportingUsersView.as_view()),
-    path('user/recommend/', views.CreateShopRecommendationView.as_view()),
-    path('user/recommend/update/', views.CreateShopRecommendationUpdateView.as_view())
-]
