467551c2f1fd8ab688e1d82257441cda2cca69ec
abhishekkushwaha4u <abhishek.kushwaha2018@vitstudent.ac.in>
2020-03-30 00:16:02 +0530

    made changes for base url for amogh

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('user/report/', views.ReportingUsersView.as_view()),
-    path('user/recommend/', views.CreateShopRecommendationView.as_view()),
-    path('user/recommend/update/', views.CreateShopRecommendationUpdateView.as_view())
+    path('report/', views.ReportingUsersView.as_view()),
+    path('recommend/', views.CreateShopRecommendationView.as_view()),
+    path('recommend/update/', views.CreateShopRecommendationUpdateView.as_view())
 ]
