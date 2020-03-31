50a7c8e7e3cd07abea40fc5692e0d8c31bddfb2e
abhishekkushwaha4u <abhishek.kushwaha2018@vitstudent.ac.in>
2020-04-01 01:35:00 +0530

    made changes to models to small fields as 100 and big as 250

--- main/models.py
+++ main/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 
 class ReportUser(models.Model):
-    user_id = models.CharField(max_length=255)
-    reported_by = models.CharField(max_length=255)
-    reason = models.CharField(max_length=300)
+    user_id = models.CharField(max_length=250)
+    reported_by = models.CharField(max_length=250)
+    reason = models.CharField(max_length=250)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
 
 
 class CreateShopRecommendation(models.Model):
-    user_id = models.CharField(max_length=255)
-    recommended_for = models.CharField(max_length=255)
+    user_id = models.CharField(max_length=250)
+    recommended_for = models.CharField(max_length=250)
     name_of_shop = models.CharField(max_length=100)
-    item = models.CharField(max_length=30)
+    item = models.CharField(max_length=100)
     phone_number = models.CharField(max_length=15, blank=True)
-    landmark = models.CharField(max_length=20)
-    extra_instruction = models.TextField(blank=True)
-    description_of_shop = models.TextField()
+    landmark = models.CharField(max_length=100)
+    extra_instruction = models.CharField(max_length=250,blank=True)
+    description_of_shop = models.CharField(max_length=250)
     read_by_user = models.BooleanField(default=0)
 
